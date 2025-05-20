497850f8ba3a539e227f7cb884e6b3720218a3e3
Eka Krisna Ferian <kresnaeka.07@gmail.com>
2025-05-21 02:50:29 +0700

    Updating Users Table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('phone');
-            $table->text('address');
+            $table->text('address')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
